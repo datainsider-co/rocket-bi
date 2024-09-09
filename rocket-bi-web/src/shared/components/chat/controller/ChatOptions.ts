f82f37d17ea2f8b3ef48aa09cea0b9934e2ad230
HaoCooky97 <hao.nguyennhat97@gmail.com>
2024-09-09 19:08:40 +0700

    Add ChatOptions.ts for Chatbot configuration interface
    This new file defines the ChatbotOptions interface, which includes various customizable properties for the chatbot's appearance and behavior. It allows setting options such as bot title, color schemes, avatar size, and messaging bubble styles.

---
+++ rocket-bi-web/src/shared/components/chat/controller/ChatOptions.ts
@@ -0,0 +1,17 @@
+export interface ChatbotOptions {
+  botTitle?: string;
+  colorScheme?: string;
+  textColor?: string;
+  bubbleBtnSize?: number;
+  animation?: boolean;
+  boardContentBg?: string;
+  botAvatarSize?: number;
+  botAvatarImg?: string;
+  msgBubbleBgBot?: string;
+  msgBubbleColorBot?: string;
+  msgBubbleBgUser?: string;
+  msgBubbleColorUser?: string;
+  inputPlaceholder?: string;
+  inputDisableBg?: string;
+  inputDisablePlaceholder?: string | null;
+}
