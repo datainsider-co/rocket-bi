68e08a2edd6a26f72aecd7c484711f6f4c10e8b9
HaoCooky97 <hao.nguyennhat97@gmail.com>
2024-09-09 19:01:37 +0700

    Add chat controller interfaces and enums
    Introduce new interfaces and enums for chat controller, including MessageAction, ChatMessageData, and OpenAiModels. These additions provide structure for handling different message types, actions, and AI model variations.

---
+++ rocket-bi-web/src/shared/components/chat/controller/MessageType.ts
@@ -0,0 +1,4 @@
+export enum MessageType {
+  text = 'text',
+  button = 'button'
+}
---
+++ rocket-bi-web/src/shared/components/chat/controller/OpenAiModels.ts
@@ -0,0 +1,5 @@
+export enum OpenAiModels {
+  Gpt4Turbo = 'gpt-4-turbo',
+  Gpt4o = 'gpt-4o',
+  GPT35Turbo = 'gpt-3.5-turbo-0125'
+}
