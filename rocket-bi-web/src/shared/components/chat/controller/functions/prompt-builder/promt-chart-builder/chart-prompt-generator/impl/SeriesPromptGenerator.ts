38a0c4a65a4db5c5ef93118baad6ef525df4e4d9
haonn <hao.nguyennhat97@gmail.com>
2024-11-18 18:43:24 +0700

    Add SeriesPromptGenerator for chart prompt functionality
    This new class extends ChartPromptGenerator and implements methods to generate and parse prompts for line charts. It forms part of the series prompt generation, handling xAxis, yAxis, legend, and sorting configurations to

---
+++ rocket-bi-web/src/shared/components/chat/controller/functions/prompt-builder/promt-chart-builder/chart-prompt-generator/impl/SeriesPromptGenerator.ts
@@ -0,0 +1,39 @@
+import { ChartPromptGenerator } from '../ChartPromptGenerator';
+import { ConfigType, FunctionTreeNode } from '@/shared';
+import { ShortInfoTableSchema } from '@/screens/chart-builder/prompt-2-chart/ChartGenerator';
+import { ChatMessageData } from '@/screens/dashboard-detail/intefaces/chatbot/ChatMessageData';
+import { ChartBuilderResponse } from '@/screens/dashboard-detail/intefaces/chatbot/prompt-builder/promt-chart-builder/ChartPromptFactory';
+import { Log } from '@core/utils';
+
+export class SeriesPromptGenerator extends ChartPromptGenerator {
+  generate(_: ShortInfoTableSchema): string {
+    return `
+    Identify present line chart:
+    - xAxis and function  to display as line.
+    - yAxis and function to display value of line.
+    - legend (optional) and function to display break of xAxis.
+    - sorting (optional) and function to sort value base on xAxis or yAxis.
+    Give me output as JSON.
+    - Top keys name: "xAxis", "yAxis", "legendOptional", "sorting" contains:
+    - "displayName": name to display.
+    ${this.sortingPrompt()}
+    ${this.fieldPrompt()}
+    ${this.functionFamilyPrompt()}
+    ${this.functionTypePrompt()}
+    `;
+  }
+
+  parse(response: ChatMessageData): ChartBuilderResponse {
+    const node = this.extractJsonFromResponse(response.text);
+    const xAxis: FunctionTreeNode[] = this.createFunctionTreeNode(ConfigType.xAxis, node);
+    const yAxis: FunctionTreeNode[] = this.createFunctionTreeNode(ConfigType.yAxis, node);
+    const legend: FunctionTreeNode[] = this.createFunctionTreeNode(ConfigType.legendOptional, node);
+    const sorting: FunctionTreeNode[] = this.createFunctionTreeNode(ConfigType.sorting, node);
+    return new Map([
+      [ConfigType.xAxis, xAxis],
+      [ConfigType.yAxis, yAxis],
+      [ConfigType.legendOptional, legend],
+      [ConfigType.sorting, sorting]
+    ]);
+  }
+}
