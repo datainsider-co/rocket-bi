8927bd9de44cde498e5c407833e48cb861a8691b
haonn <hao.nguyennhat97@gmail.com>
2024-11-19 13:38:15 +0700

    Add PromptEvents and QueryEditorEvents enums
    Introduce PromptEvents enum to manage prompt-related events and QueryEditorEvents enum for handling text appending in the query editor. This addition aims to improve event handling consistency and maintainability across the application.

---
+++ rocket-bi-web/src/shared/enums/PromptEvents.ts
@@ -0,0 +1,9 @@
+export enum PromptEvents {
+  submitPrompt = 'submit_prompt',
+  analyzingPrompt = 'analyzing_prompt',
+  analyzePromptCompleted = 'analyze_prompt_completed'
+}
+
+export enum QueryEditorEvents {
+  appendText = 'append_text'
+}
